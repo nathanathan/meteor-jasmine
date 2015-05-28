e0422709bb50c3bb350072a471e584684f674b11
Nathan Breit <nabreit@gmail.com>
2015-05-28 18:14:47 +0800

    Update run.sh

--- test-app/run.sh
+++ test-app/run.sh
@@ -5,5 +5,6 @@
 #export LONG_RUNNING_CHILD_PROCESS_LOG_LEVEL=debug
 #export VELOCITY_USE_CHECKED_OUT_METEOR=1
 
+VELOCITY_TEST_PACKAGES=1 meteor test-packages --driver-package velocity:html-reporter --velocity package-to-test
 export JASMINE_PACKAGES_TO_INCLUDE_IN_UNIT_TESTS=package-to-include
 meteor --test --settings settings.json --release velocity:METEOR@1.1.0.2_3
